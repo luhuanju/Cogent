1417742f0d27b007965880cb70dda9078fa2acef
HUANJU.LU <huanjulu@huanjus-MBP.localdomain>
2023-02-07 13:31:12 -0800

    task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -5,5 +5,36 @@ public class Task {
 		// TODO Auto-generated method stub
 
 	}
+	
+	
+	
+	public void helperOperator() {
+		
+		//Arithmetic
+		System.out.println(1+2);
+		
+		//Assignment
+		int a=0;
+		System.out.println(a+=1);
+		
+		//Shift
+		System.out.println(123>>2);
+		
+		//Relational
+		System.out.println(a <=2 );
+		
+		//Bitwise
+		System.out.println(1&1);
+		
+		//Logical
+		System.out.println(1==1 && 2==2);
+		
+		//Unary
+		System.out.println(a++);
+		
+		//Ternary
+		System.out.println(1==1? 0 : 1);
+		
+	}
 
 }
